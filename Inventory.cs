5a4796a5588f26df8925cf7f6ee0c38280cae527
shankar Ghimire <ssghimire@gmail.com>
2021-06-06 17:23:51 -0400

    Adds Inventory class to the project

---
+++ Inventory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRepairManagementSystem
+{
+    class Inventory
+    {
+        private string id;
+        private string vehicleId;
+        private int numberOnHand;
+        private decimal price;
+        private decimal cost;
+
+        public string Id
+        {
+            get { return id; }
+            set { this.id = value; }
+        }
+
+        public string VehicleId
+        {
+            get { return vehicleId; }
+            set { this.vehicleId = value; }
+        }
+
+        public int NumberOnHand
+        {
+            get { return numberOnHand; }
+            set { this.numberOnHand = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { this.price = value; }
+        }
+
+        public decimal Cost
+        {
+            get { return cost; }
+            set { this.cost = value; }
+        }
+
+        //No-argument constructor
+        public Inventory() { }
+
+        //Parameterized constructor
+        public Inventory(string id, string vehicleId, int numberOnHand, decimal price, decimal cost)
+        {
+            Id = id;
+            VehicleId = vehicleId;
+            NumberOnHand = numberOnHand;
+            Price = price;
+            Cost = cost;
+        }
+
+        
+
+
+    }
+}
